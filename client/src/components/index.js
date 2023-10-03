e9b7ad59f53f14fcb52d4a1d62042e1626a341e7
Ramirof <ramirofiscella@gmail.com>
2023-10-03 19:03:07 -0300

    typo

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -3,7 +3,7 @@ import CardContainer from "./CardContainer/CardContainer";
 import CountryCard from "./CountryCard/CountryCard";
 import SearchBar from "./SearchBar/SearchBar";
 import ContinentFilter from "./ContinentFilter/ContinentFilter";
-import PopulationSort from "./populationSort/populationSort";
+import PopulationSort from "./PopulationSort/PopulationSort";
 import AlphSort from "./AlphSort/AlphSort";
 
 export {
