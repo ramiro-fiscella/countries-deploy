75bcc3a9518a9f777538b2700051e41be088874a
Ramirof <ramirofiscella@gmail.com>
2023-10-03 20:14:47 -0300

    typo

--- api/index.js
+++ api/index.js
@@ -2,13 +2,13 @@ require("dotenv").config();
 const server = require("./src/server");
 const { conn } = require("./src/db.js");
 
-const DB_PORT = process.env.DB_PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
 conn
   .sync({ force: false })
   .then(() => {
-    server.listen(DB_PORT, () => {
-      console.log(`Server listening on DB_PORT ${DB_PORT}`);
+    server.listen(PORT, () => {
+      console.log(`Server listening on port ${PORT}`);
     });
   })
   .catch((error) => console.log("Error while setting up the server: ", error));
