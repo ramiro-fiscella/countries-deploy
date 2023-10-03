aa76f52bcb02445bdb7edbd188490ff380de5955
Ramirof <ramirofiscella@gmail.com>
2023-10-03 20:21:07 -0300

    typos

--- api/src/routes/countries.routes.js
+++ api/src/routes/countries.routes.js
@@ -1,12 +1,12 @@
 const { Router } = require("express");
 const router = Router();
-//traer controlladores y configurar rutas
+// traer controlladores y configurar rutas
 const { countriesSync } = require("../controllers/countriesSync");
 const { getCountries } = require("../controllers/getCountries");
 const { getCountryById } = require("../controllers/getCountryById");
 const { getCountryByName } = require("../controllers/getCountryByName");
 
-// router.get("/", countriesSync);
+router.get("/", countriesSync);
 router.get("/countries", getCountries);
 router.get("/countries/:id", getCountryById);
 router.get("/countries/name", getCountryByName);
